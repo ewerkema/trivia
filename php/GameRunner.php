5612cb63508cd0e40c31214f12d267546e30a04c
Enrico Werkema <enricowerkema@gmail.com>
2020-06-01 12:33:31 +0200

    Refactor indentation GameRunner

--- php/GameRunner.php
+++ php/GameRunner.php
@@ -1,27 +1,25 @@
 <?php
 
-include __DIR__.'/app/Game.php';
+include __DIR__ . '/app/Game.php';
 
 $notAWinner;
 
-  $aGame = new \App\Game();
-  
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
-  
-  
-  do {
-    
-    $aGame->roll(rand(0,5) + 1);
-    
-    if (rand(0,9) == 7) {
-      $notAWinner = $aGame->wrongAnswer();
+$aGame = new \App\Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+
+do {
+    $aGame->roll(rand(0, 5) + 1);
+
+    if (rand(0, 9) == 7) {
+        $notAWinner = $aGame->wrongAnswer();
     } else {
-      $notAWinner = $aGame->wasCorrectlyAnswered();
+        $notAWinner = $aGame->wasCorrectlyAnswered();
     }
-    
-    
-    
-  } while ($notAWinner);
+
+
+} while ($notAWinner);
   
