690d9cd35ba7643e4744e8bd57533ba429aaea28
Enrico Werkema <enricowerkema@gmail.com>
2020-06-01 11:15:29 +0200

    Add PHPUnit and game initialization tests

---
+++ php/tests/GameInitializationTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests;
+
+include __DIR__.'/../Game.php';
+
+use Game;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @property Game game
+ */
+class GameInitializationTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->game = new Game();
+    }
+
+    /** @test */
+    public function the_game_can_add_players()
+    {
+        $this->game->add("John");
+
+        $this->assertEquals(1, $this->game->howManyPlayers());
+
+        $this->game->add("Doe");
+
+        $this->assertEquals(2, $this->game->howManyPlayers());
+    }
+
+    /** @test */
+    public function the_game_is_playable_from_2_players()
+    {
+        $this->assertFalse($this->game->isPlayable());
+
+        $this->game->add("John");
+
+        $this->assertFalse($this->game->isPlayable());
+
+        $this->game->add("Doe");
+
+        $this->assertTrue($this->game->isPlayable());
+    }
+    
+    /** @test */
+    public function the_game_initializes_questions()
+    {
+        $this->assertNotCount(0, $this->game->popQuestions);
+        $this->assertNotCount(0, $this->game->rockQuestions);
+        $this->assertNotCount(0, $this->game->scienceQuestions);
+        $this->assertNotCount(0, $this->game->sportsQuestions);
+    }
+}
